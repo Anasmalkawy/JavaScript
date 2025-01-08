ad05b0bd06bfe6e08e2f2f752c265e98795c1012
anasmalkawy <anas.malkawy1999@gmail.com>
2025-01-08 20:41:37 +0300

    a

---
+++ 8.1.2025 (api and json)/main.js
@@ -0,0 +1,44 @@
+async function disdata1() {
+  let url = ('https://api.npoint.io/20622dc81d2be83a1380')
+  const response = await fetch(url);
+    const data = await response.json();
+    const outputDiv = document.getElementById('output');
+
+
+    for (const user of data) {
+        const userCard = document.createElement('div');
+        userCard.classList.add("card")
+        userCard.innerHTML = `
+          <h3>${user.name}</h3>
+          <p>Email: ${user.email}</p>
+          <p>age: ${user.age}</p>
+          <p>prof:${user.profession}</p><hr><br>
+        `;
+        outputDiv.appendChild(userCard);
+    }
+}
+
+
+
+
+
+async function disdata2() {
+  const response1 = await fetch('data.json');
+    const data1 = await response1.json();
+    const outputDiv1 = document.getElementById('output1');
+
+
+    for (const user1 of data1) {
+        const userCard = document.createElement('div');
+        userCard.classList.add("card1")
+        userCard.innerHTML = `
+          <h3>${user1.name}</h3>
+          <p>Email: ${user1.email}</p>
+          <p>age: ${user1.age}</p>
+          <p>prof:${user1.profession}</p><hr><br>
+        `;
+        outputDiv1.appendChild(userCard);
+    }
+}
+
+
